3b2079ade652fb0f7a26099c952da88d109df428
Heaven Volkoff <vasconcellos.dev@gmail.com>
2020-05-01 01:15:20 -0300

    Bump version to 0.13.1  - Fix emitter.wait(...) not removing internal future listener when an error occurs

--- emitter/_wait.py
+++ emitter/_wait.py
@@ -3,7 +3,8 @@ import typing as T
 
 # Project
 from ._on import on
-from ._helpers import get_running_loop
+from ._remove import remove
+from ._helpers import get_running_loop, retrieve_loop_from_listener
 
 # Type generics
 K = T.TypeVar("K")
@@ -17,10 +18,16 @@ async def wait(event: T.Union[str, T.Type[K]], namespace: object) -> K:
     # Retrieve listeners
     loop = get_running_loop()
     result: "Future[K]" = loop.create_future()
+    listeners = retrieve_loop_from_listener(namespace)
 
-    on(event, namespace, result.set_result, once=True)
-
-    # Don't keep reference
+    # Don't keep namespace reference
     del namespace
 
-    return await result
+    on(event, listeners, result.set_result, once=True)
+
+    try:
+        return await result
+    finally:
+        if result.exception():
+            # An exception occurred. Probably that the listeners didn't execute, so remove it.
+            remove(event, listeners, result.set_result)
